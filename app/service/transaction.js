66f6d9cf5771157f4ea92d4a36c8355407a7a69d
ziqian9206 <ziqian9206@126.com>
2019-03-16 00:54:56 +0800

    交易记录存储

--- app/service/transaction.js
+++ app/service/transaction.js
@@ -15,6 +15,26 @@ class TransactionService extends Service {
     transaction.success = success;
     return transaction.save();
   }
+
+  record({ uid, sid, sname, action, count, price, totalFund, earning, rate, time }) {
+    const transaction = new this.ctx.model.Transaction();
+    transaction.uid = uid;
+    transaction.sid = sid;
+    transaction.sname = sname;
+    transaction.action = action;
+    transaction.count = count;
+    transaction.price = price;
+    transaction.success = 1; // 模拟记录总是成功
+    transaction.totalFund = totalFund;
+    transaction.earning = earning;
+    transaction.rate = rate;
+    transaction.time = time;
+    return transaction.save();
+  }
+
+  getTransactionByUid({uid}) {
+    return this.ctx.model.Transaction.find({ uid });
+  }
 }
 
 module.exports = TransactionService;
