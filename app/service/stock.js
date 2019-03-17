4cb0afcb82c74dd53c4c7890be48131c3ac99b42
ziqian9206 <ziqian9206@126.com>
2019-03-18 00:10:35 +0800

    新增及修改股票service实现逻辑

--- app/service/stock.js
+++ app/service/stock.js
@@ -6,22 +6,38 @@ const Service = require('egg').Service;
 class StockService extends Service {
   // 获取用户持股
   getUserStocks(uid) {
-    return this.ctx.model.Stock.findOne({ uid });
+    return this.ctx.model.Stock.find({ uid });
+  }
+
+  getUserStockById({ uid, sid }) {
+    return this.ctx.model.Stock.find({ uid, sid });
   }
 
   getStockInfo(sid) {
     return this.ctx.curl(`http://hq.sinajs.cn/list=${sid}`);
   }
 
-  changeUserStocks({ uid, symbol, name, held, transactionTime }) {
+  changeUserStocks({ type, uid, symbol, name, hold, earning, transactionTime }) {
     const stock = this.ctx.model.Stock();
-    stock.uid = uid;
-    stock.symbol = symbol;
-    stock.name = name;
-    stock.held = held;
-    stock.transactionTime = transactionTime;
-    console.log('changeUserStocks:::::>', uid, symbol, name, typeof held, transactionTime, stock);
-    return stock.save();
+    if (type === 1) {
+      stock.uid = uid;
+      stock.sid = symbol;
+      stock.name = name;
+      stock.hold = hold;
+      stock.earning = earning;
+      stock.transactionTime = transactionTime;
+      console.log('changeUserStocks:::::>', uid, symbol, name, typeof hold, transactionTime, stock);
+      return stock.save();
+    }
+  }
+
+  updateUserStock({ uid, symbol, hold, earning, transactionTime }) {
+    console.log('updateUserStock:', uid, symbol, hold, earning, transactionTime);
+    return this.ctx.model.Stock.updateOne({ uid, symbol, hold, earning, transactionTime });
+  }
+
+  removeUserStock({ uid, symbol }) {
+    return this.ctx.model.Stock.remove({ uid, symbol });
   }
 }
 
