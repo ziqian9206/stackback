d27ea6e4c855eb7625f97fa2207edd16722d8702
ziqian9206 <ziqian9206@126.com>
2019-03-18 00:09:24 +0800

    当前持仓，当前委托接口实现

--- app/controller/stock.js
+++ app/controller/stock.js
@@ -3,6 +3,11 @@
 const Controller = require('egg').Controller;
 const iconv = require('iconv-lite');
 
+const MongoObjectIdSchema = {
+  type: 'string',
+  format: /^[0-9a-f]{24}$/i,
+};
+
 class HomeController extends Controller {
 
   async info() {
@@ -18,14 +23,49 @@ class HomeController extends Controller {
       ctx.helper.error({ ctx, error: 101, msg: '没有查询到该股票' });
     } else {
       const info = {
-        symbol: stockInfoGBK[0].match(/(sh|sz)\d+/g)[0], // 0: 股票代码
+        sid: stockInfoGBK[0].match(/(sh|sz)\d+/g)[0], // 0: 股票代码
         name: stockInfoGBK[0].match(/[\u4E00-\u9FA5]+/g)[0], // 0: 股票名字
+        yesterdayEnd: stockInfoGBK[2], // 昨日收盘价
         trade: stockInfoGBK[1], // 今日开盘价格
         currentPrice: stockInfoGBK[3], // 当前价格
         time: `${stockInfoGBK[30]} ${stockInfoGBK[31]}`, // 时间
       };
       ctx.helper.success({ ctx, res: info });
     }
+  }
+
+  async hold() {
+    const { ctx } = this;
+    const uid = ctx.params.uid;
+    if (!uid) {
+      ctx.helper.error({ ctx, msg: 'uid不能为空' });
+    }
+
+    const res = await ctx.service.transaction.getUserHoldByUid({ uid });
+    ctx.helper.success({ ctx, res });
+  }
+
+
+  async commission() {
+    const { ctx } = this;
+    console.log('==========> commission:', ctx.params, ctx.query);
+    const uid = ctx.params.uid;
+    if (!uid) {
+      ctx.helper.error({ ctx, msg: 'uid不能为空' });
+    }
+    ctx.validate({ uid: MongoObjectIdSchema }, ctx.params);
+
+    const params = {};
+
+    params.uid = uid;
+
+    if (ctx.query.hasOwnProperty('starttime') && ctx.query.hasOwnProperty('endtime')) {
+      params.starttime = ctx.query.starttime;
+      params.endtime = ctx.query.endtime;
+    }
+
+    const res = await ctx.service.transaction.getUserCommissionByUid(params);
+    ctx.helper.success({ ctx, res });
   }
 }
 
